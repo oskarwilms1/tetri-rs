8ad9f940631cafbbc79b24e01a80760b5e28c134
oskarwilms1 <o.wilms.062@studms.ug.edu.pl>
2026-01-12 16:55:14 +0100

    Clean up

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,4 @@
 use bevy::prelude::*;
-use bevy_inspector_egui::prelude::ReflectInspectorOptions;
-use bevy_inspector_egui::quick::WorldInspectorPlugin;
-
-use bevy_egui::EguiPlugin;
-use bevy_inspector_egui::InspectorOptions;
 mod board;
 mod config;
 mod plugins;
@@ -13,13 +8,7 @@ use crate::plugins::{
     gravity_plugin::GravityPlugin, observers_plugin::ObserversPlugin,
     startup_plugin::StartupPlugin,
 };
-#[derive(Reflect, Resource, Default, InspectorOptions)]
-#[reflect(Resource, InspectorOptions)]
-struct Configuration {
-    name: String,
-    #[inspector(min = 0.0, max = 1.0)]
-    option: f32,
-}
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins.set(WindowPlugin {
@@ -37,10 +26,6 @@ fn main() {
             }),
             ..default()
         }))
-        .add_plugins(EguiPlugin::default())
-        .init_resource::<Configuration>()
-        .register_type::<Configuration>()
-        .add_plugins(WorldInspectorPlugin::new())
         .add_plugins(AssetsPlugin)
         .add_plugins(ObserversPlugin)
         .add_plugins(StartupPlugin)
